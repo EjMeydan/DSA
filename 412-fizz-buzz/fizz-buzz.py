13ba5deea13ee085e36593c327f88973de25a78b
EjMeydan <ej.meydan@outlook.com>
2025-08-15 15:55:56 +0100

    Time: 0 ms (100.00%) | Memory: 18.2 MB (69.59%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        output = []
+
+        for i in range(1, n+1):
+            if i % 3 == 0:
+                answer = "Fizz"
+                if i % 5 == 0:
+                    answer += "Buzz"
+            elif i % 5 == 0:
+                answer = "Buzz"
+            else:
+                answer = str(i)
+            
+            output.append(answer)
+        return output
+            
+
+                
