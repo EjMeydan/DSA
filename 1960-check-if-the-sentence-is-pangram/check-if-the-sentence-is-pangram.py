0d629084f6c520527076a1c5e8f908fc91986153
EjMeydan <ej.meydan@outlook.com>
2025-09-21 03:37:24 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (9.54%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,12 +1,7 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         
-        letters = {}
-
-        for char in sentence:
-            if char not in letters:
-                letters[char] = 1
-        
-        if len(letters) >= 26:
+        letters = set(sentence)
+        if len(letters) == 26:
             return True
         return False
